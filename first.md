2b34ab0523b78c0c03a1b65691d92676c49e3614
595money_ubunutu <595money@gmail.com>
2020-05-02 17:39:16 +0800

    Init repo

---
+++ first.md
@@ -0,0 +1,22 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,py:percent,md
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.2'
+      jupytext_version: 1.4.2
+  kernelspec:
+    display_name: Python 3
+    language: python
+    name: python3
+---
+
+```python
+from jupyterthemes import jtplot
+from IPython.core.display import display, HTML
+
+display(HTML("<style>.container { width:80% !important; }</style>"))
+jtplot.style()
+```
---
+++ first.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent,md
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from jupyterthemes import jtplot
+from IPython.core.display import display, HTML
+
+display(HTML("<style>.container { width:80% !important; }</style>"))
+jtplot.style()
